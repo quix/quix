d08213b2e75f5a057dc275d9155f165a73f02178
quix <quixoticsycophant@gmail.com>
2009-03-14 11:40:38 -0400

    add thread_local singletons; tweak loop_with

---
+++ test/test_thread_local.rb
@@ -0,0 +1,30 @@
+$LOAD_PATH.unshift File.dirname(__FILE__) + "/../lib"
+
+require 'test/unit'
+require 'quix/thread_local'
+
+class TestThreadLocal < Test::Unit::TestCase
+  def test_unique
+    a = Quix::ThreadLocal.new { Hash.new }
+    a.value[:x] = 33
+    other_value = nil
+    Thread.new {
+      a.value[:x] = 44
+      other_value = a.value[:x]
+    }.join
+    assert_equal(33, a.value[:x])
+    assert_equal(44, other_value)
+  end
+  
+  class A
+    include Quix::ThreadLocal.accessor_module(:x) { 33 }
+  end
+
+  def test_accessor
+    a = A.new
+    assert_equal(33, a.x)
+    a.x = 44
+    assert_equal(44, a.x)
+  end
+end
+
