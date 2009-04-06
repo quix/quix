96d2a39dfc9b68bf21d1b5e877f837ced528a1d2
quix <quixoticsycophant@gmail.com>
2009-04-05 20:44:31 -0400

    add Module#define_module_function et al

--- lib/quix/module.rb
+++ lib/quix/module.rb
@@ -1,5 +1,26 @@
 
 class Module
+  #
+  # define_public_method
+  # define_protectd_method
+  # define_private_method
+  # define_module_function
+  #
+  [:public, :protected, :private, :module_function].each { |scope|
+    name = scope == :module_function ? scope : "#{scope}_method"
+    # TODO: jettison 1.8.6, remove eval and use |&block|
+    eval %{
+      def define_#{name}(name, &block)
+        define_method(name, &block)
+        #{scope}(name)
+      end
+    }
+  }
+
+  #
+  # Inside a +polite+ block, a method definition is ignored if the
+  # method already exists.
+  #
   def polite(&block)
     added =       Hash.new { |hash, key| hash[key] = Array.new }
     scope_cache = Hash.new { |hash, key| hash[key] = Array.new }
