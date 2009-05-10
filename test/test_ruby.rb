f781bcfe90967502effac69f2cc0da1f1fccc388
quix <quixoticsycophant@gmail.com>
2009-05-10 12:42:06 -0400

    add test_ruby

---
+++ test/test_ruby.rb
@@ -0,0 +1,40 @@
+require File.dirname(__FILE__) + "/common"
+
+require 'stringio'
+require 'quix/ruby'
+
+class TestRuby < Test::Unit::TestCase
+  include TestDataDir
+  Ruby = Quix::Ruby
+
+  def test_1
+    assert File.directory?(DATA_DIR)
+    assert_nothing_raised {
+      Ruby.run("-rfileutils", "-e", "FileUtils.touch '#{DATA_DIR}/z'")
+    }
+    assert File.exist?("#{DATA_DIR}/z")
+    assert_raises(RuntimeError) {
+      Ruby.run("-e", "$stderr.close ; raise")
+    }
+  end
+
+  def test_2
+    unless RUBY_PLATFORM =~ %r!(mswin|cygwin|mingw)!
+      previous = Config::CONFIG["host"]
+      begin
+        Config::CONFIG["host"] = "mingw"
+        Ruby.no_warnings {
+          Ruby.module_eval {
+            remove_const :EXECUTABLE
+          }
+          load File.dirname(__FILE__) + "/../lib/quix/ruby.rb"
+        }
+        assert_nothing_raised {
+          Ruby.run("-e", "")
+        }
+      ensure
+        Config::CONFIG["host"] = previous
+      end
+    end
+  end
+end
