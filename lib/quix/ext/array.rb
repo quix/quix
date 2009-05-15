699e12ba8a1401626f125eb4860b3da7f3ffd63e
quix <quixoticsycophant@gmail.com>
2009-05-15 08:36:59 -0400

    add methods second, third, ... to array.rb

--- lib/quix/ext/array.rb
+++ lib/quix/ext/array.rb
@@ -7,4 +7,23 @@ class Array
   def tail
     self[1..-1]
   end
+
+  [
+    nil,
+    :second,
+    :third,
+    :fourth,
+    :fifth,
+    :sixth,
+    :seventh,
+    :eighth,
+    :ninth,
+    :tenth,
+  ].each_with_index { |method, index|
+    if method
+      define_method method do
+        self[index]
+      end
+    end
+  }
 end
--- test/test_array.rb
+++ test/test_array.rb
@@ -7,5 +7,17 @@ class TestArray < Test::Unit::TestCase
     a = [3, 4, 5]
     assert_equal a.first, a.head
     assert_equal [4, 5], a.tail
+
+    t = %w[a b c d e f g h i j].map { |t| t.to_sym }
+    assert_equal :a, t.first
+    assert_equal :b, t.second
+    assert_equal :c, t.third
+    assert_equal :d, t.fourth
+    assert_equal :e, t.fifth
+    assert_equal :f, t.sixth
+    assert_equal :g, t.seventh
+    assert_equal :h, t.eighth
+    assert_equal :i, t.ninth
+    assert_equal :j, t.tenth
   end
 end
