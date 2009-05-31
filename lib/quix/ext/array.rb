09a8228b65e1df8df8705dcb03c904a1164cd6c2
quix <quixoticsycophant@gmail.com>
2009-05-31 18:38:41 -0400

    add Array#split and Array#penultimate

--- lib/quix/ext/array.rb
+++ lib/quix/ext/array.rb
@@ -26,4 +26,54 @@ class Array
       end
     end
   }
+
+  def penultimate
+    self[-2]
+  end
+
+  #
+  # Array#split(:capture => true) imitates the behavior of
+  # String#split with a regexp capture:
+  #
+  #   "abcd".split %r!(a)! #=> ["", "a", "bcd"]
+  #   %w[a b c d].split "a", :capture => true #=> [[], "a", ["b", "c", "d"]]
+  #
+  #   "abcd".split %r!(b)! #=> ["a", "b", "cd"]
+  #   %w[a b c d].split "b", :capture => true #=> [["a"], "b", ["c", "d"]]
+  #
+  #   "abcd".split %r!(c)! #=> ["ab", "c", "d"]
+  #   %w[a b c d].split "c", :capture => true #=> [["a", "b"], "c", ["d"]]
+  #
+  #   "abcd".split %r!(d)! #=> ["abc", "d"]
+  #   %w[a b c d].split "d", :capture => true #=> [["a", "b", "c"], "d"]
+  #
+  #   "a".split %r!(a)! #=> ["", "a"]
+  #   %w[a].split "a", :capture => true #=> [[], "a"]
+  #
+  def split(*args)
+    opts = args.last.is_a?(Hash) ? args.pop : Hash.new
+    block_given = block_given?
+
+    if block_given
+      unless args.size == 0
+        raise ArgumentError, "cannot pass both value and block"
+      end
+    elsif args.size != 1
+      raise ArgumentError, "wrong number of arguments (#{args.size} for 1)"
+    end
+
+    capture = opts[:capture]
+    value = args.first
+
+    result = [[]]
+    each_with_index { |elem, index|
+      if (block_given and yield(elem)) or (not block_given and elem == value)
+        result << elem if capture
+        result << Array.new unless index == size - 1
+      else
+        result.last << elem
+      end
+    }
+    result
+  end
 end
--- test/test_array.rb
+++ test/test_array.rb
@@ -3,12 +3,21 @@ require File.dirname(__FILE__) + "/common"
 require "quix/ext/array"
 
 class TestArray < Test::Unit::TestCase
-  def test_array
+  def test_head_tail
     a = [3, 4, 5]
-    assert_equal a.first, a.head
+    assert_equal 3, a.head
     assert_equal [4, 5], a.tail
 
+    assert_equal 3, [3].head
+    assert_equal nil, [].head
+
+    assert_equal nil, [].tail
+    assert_equal [], [3].tail
+  end
+    
+  def test_ords
     t = %w[a b c d e f g h i j].map { |t| t.to_sym }
+
     assert_equal :a, t.first
     assert_equal :b, t.second
     assert_equal :c, t.third
@@ -19,5 +28,108 @@ class TestArray < Test::Unit::TestCase
     assert_equal :h, t.eighth
     assert_equal :i, t.ninth
     assert_equal :j, t.tenth
+
+    assert_equal :i, t.penultimate
+    assert_equal nil, [].penultimate
+    assert_equal nil, [:z].penultimate
+    assert_equal :y, [:y, :z].penultimate
+  end
+
+  def test_split_1
+    s = %w[a b c d e f].split("d")
+    assert_equal [["a", "b", "c"], ["e", "f"]], s
+
+    s = %w[a b c d e f].split("a")
+    assert_equal [[], %w[b c d e f]], s
+
+    s = %w[a b c d e f].split("f")
+    assert_equal [%w[a b c d e]], s
+
+    s = ["a"].split("a")
+    assert_equal [[]], s
+  end
+
+  def test_split_2
+    s = %w[a b c d e f].split("d", :capture => true)
+    assert_equal [["a", "b", "c"], "d", ["e", "f"]], s
+
+    s = %w[a b c d e f].split("a", :capture => true)
+    assert_equal [[], "a", %w[b c d e f]], s
+
+    s = %w[a b c d e f].split("f", :capture => true)
+    assert_equal [%w[a b c d e], "f"], s
+
+    s = ["a"].split("a", :capture => true)
+    assert_equal [[], "a"], s
+  end
+
+  def test_split_3
+    s = %w[a b c d e f].split { |t| t == "d" }
+    assert_equal [["a", "b", "c"], ["e", "f"]], s
+
+    s = %w[a b c d e f].split { |t| t == "a" }
+    assert_equal [[], %w[b c d e f]], s
+
+    s = %w[a b c d e f].split { |t| t == "f" }
+    assert_equal [%w[a b c d e]], s
+
+    s = ["a"].split { |t| t == "a" }
+    assert_equal [[]], s
+
+    s = [].split { flunk }
+    assert_equal [[]], s
+  end
+
+  def test_split_4
+    s = %w[a b c d e f].split(:capture => true) { |t| t == "d" }
+    assert_equal [["a", "b", "c"], "d", ["e", "f"]], s
+
+    s = %w[a b c d e f].split(:capture => true) { |t| t == "a" }
+    assert_equal [[], "a", %w[b c d e f]], s
+
+    s = %w[a b c d e f].split(:capture => true) { |t| t == "f" }
+    assert_equal [%w[a b c d e], "f"], s
+
+    s = ["a"].split(:capture => true) { |t| t == "a" }
+    assert_equal [[], "a"], s
+
+    s = [].split(:capture => true) { flunk }
+    assert_equal [[]], s
+  end
+  
+  def test_split_5
+    assert_equal ["", "a", "", "a"], "aa".split(%r!(a)!)
+    assert_equal [[], "a", [], "a"], %w[a a].split("a", :capture => true)
+
+    assert_equal ["", "a", "", "a", "b"],
+                 "aab".split(%r!(a)!)
+    assert_equal [[], "a", [], "a", ["b"]],
+                 %w[a a b].split("a", :capture => true)
+
+    assert_equal [[], [1, 2], [4, 5]], (0..5).to_a.split { |t| t % 3 == 0 }
+  end
+
+  def test_split_6
+    assert_raises(ArgumentError) {
+      [].split
+    }
+    assert_raises(ArgumentError) {
+      [].split(:capture => true)
+    }
+    assert_nothing_raised {
+      [].split("a")
+    }
+    assert_raises(ArgumentError) {
+      [].split("a") { }
+    }
+    assert_raises(ArgumentError) {
+      [].split("a", "b")
+    }
+    assert_raises(ArgumentError) {
+      [].split("a", :capture => true) { }
+    }
+    assert_raises(ArgumentError) {
+      [].split("a", "b", :capture => true)
+    }
   end
 end
