b142e2f9cd4511cd0f15161769d5b5b00894115b
quix <quixoticsycophant@gmail.com>
2009-06-21 17:40:26 -0400

    add Kernel#split_options

--- lib/quix/ext/kernel.rb
+++ lib/quix/ext/kernel.rb
@@ -7,4 +7,12 @@ module Kernel
       raise "sys(#{args_str}) failed with status #{$?.exitstatus}"
     end
   end
+
+  def split_options(args)
+    if args.last.is_a? Hash
+      [args[0..-2], args.last]
+    else
+      [args, Hash.new]
+    end
+  end
 end
