d0b4061e201a577db577e628cbe14236d14eba37
quix <quixoticsycophant@gmail.com>
2009-07-20 00:07:51 -0400

    optimize eval in module.rb

--- lib/quix/ext/module.rb
+++ lib/quix/ext/module.rb
@@ -2,18 +2,26 @@
 class Module
   #
   # define_public_method
-  # define_protectd_method
+  # define_protected_method
   # define_private_method
   # define_module_function
   #
-  [:public, :protected, :private, :module_function].each { |scope|
+
+  # TODO: jettison 1.8.6; remove eval and use |&block|
+  code = [
+    :public,
+    :protected,
+    :private,
+    :module_function
+  ].inject("") { |acc, scope|
     name = scope == :module_function ? scope : "#{scope}_method"
-    # TODO: jettison 1.8.6, remove eval and use |&block|
-    eval <<-end_eval
+    # TODO: fix rcov %{} bug
+    acc << %^
       def define_#{name}(name, &block)
         define_method(name, &block)
         #{scope}(name)
       end
-    end_eval
+    ^
   }
+  eval(code)
 end
